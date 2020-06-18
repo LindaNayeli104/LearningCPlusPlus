8e751e66b14a372a7772d2f3ed88dedabf7227fb
LindaNayeli104 <linda_nayeli104@hotmail.com>
2020-06-18 13:26:51 -0500

    Project finished

--- ProyectoLaberinto.cpp
+++ ProyectoLaberinto.cpp
@@ -5,11 +5,11 @@ using namespace std;
 void drawMap(int heroPosX, int heroPosY, char gameMap[5][5]){
     for (int i = 0; i < 5; i++){
         for (int j = 0; j < 5; j++){   
-            if(i != heroPosY){                              //columnas
-                cout << gameMap[j][i];
+            if(i != heroPosY){                              //filas
+                cout << gameMap[i][j];
             }else{
-                if(j != heroPosX){                            //filas
-                    cout << gameMap[j][i];
+                if(j != heroPosX){                            //columnas
+                    cout << gameMap[i][j];
                 }else{
                     cout << 'H';
                 }
@@ -22,7 +22,7 @@ void drawMap(int heroPosX, int heroPosY, char gameMap[5][5]){
 int main(){
     
     int heroPosX = 1;
-    int heroPosY = 1;
+    int heroPosY = 0;
     bool isGameOver = false;
     char input = ' ';
 
@@ -45,9 +45,9 @@ int main(){
         }else if(input == 'a'){
             heroPosX--;
         }else if(input == 'w'){
-            heroPosY++;
-        }else if(input == 'z'){
             heroPosY--;
+        }else if(input == 'z'){
+            heroPosY++;
              
         }else if(input == 'p'){
             isGameOver = true;
